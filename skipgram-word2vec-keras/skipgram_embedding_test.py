5fe2c36e1c2fceb5f163b529a1687c8a81be8ec3
ping <ping@DESKTOP-16URGOI>
2017-07-24 16:50:43 +0800

    add a test py file

---
+++ skipgram-word2vec-keras/skipgram_embedding_test.py
@@ -0,0 +1,17 @@
+﻿# -*- coding: utf-8 -*-
+from gensim import models
+from os import path
+
+import utils
+import cilin
+from cilin import CilinSimilarity
+from ioFile import dataFromFile
+
+
+# evaluate using gensim
+test_word_list = [u'乙型', u'肝炎', u'肝癌']
+for word in test_word_list:
+	print word
+	utils.most_similar(positive=[word], negative=[])
+
+
